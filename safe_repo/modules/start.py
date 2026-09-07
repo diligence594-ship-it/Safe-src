9a6333172fc8c43aeda111de0649487474837a7f
diligence594-ship-it <diligence594@gmail.com>
2026-09-07 13:09:34 +0530

    Add files via upload

--- safe_repo/modules/start.py
+++ safe_repo/modules/start.py
@@ -2,43 +2,22 @@ from pyrogram import filters
 from safe_repo import app
 from safe_repo.core import script
 from safe_repo.core.func import subscribe
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import OWNER_ID
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
-
-# ------------------- Start Buttons ------------------- #
+# ------------------- Start-Buttons ------------------- #
 
 buttons = InlineKeyboardMarkup(
     [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url="https://t.me/safe_repo"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                "Buy Premium",
-                url="https://t.me/safe_repo_bot"
-            )
-        ]
+        [InlineKeyboardButton("Join Channel", url="https://t.me/safe_repo")],
+        [InlineKeyboardButton("Buy Premium", url="https://t.me/safe_repo_bot")]
     ]
 )
 
-
-# ------------------- /start Command ------------------- #
-
 @app.on_message(filters.command("start"))
 async def start(_, message):
-
     join = await subscribe(_, message)
-
     if join == 1:
         return
-
-    await message.reply_text(
-        text=script.START_TXT.format(
-            message.from_user.mention
-        ),
-        reply_markup=buttons
-    )
-```
+    await message.reply_text(text=script.START_TXT.format(message.from_user.mention), 
+                              reply_markup=buttons)
